3ecec7cbe80d4f4deea5f1c55d6535d485e08e13
TJ <thaddeusjiang@gmail.com>
2022-07-03 21:45:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Auto Play
+
+## Features
+
+- [x] TeamSpirit automatically stamped every day
+
+## TeamSpirit automatically stamped every day
+
+### Usage
+
+1. fork this repository
+2. setup Secret
+
+```
+PLAYWRIGHT_TEST_BASE_URL=<your company teamspirit url>
+USERNAME=
+PASSWORD=
+```
---
+++ tests/start-work.spec.ts
@@ -0,0 +1,11 @@
+import { test } from "@playwright/test";
+
+test("click start", async ({ page }) => {
+  await page.goto(process.env.PLAYWRIGHT_TEST_BASE_URL ?? "");
+  await page.type("#username", process.env.USERNAME ?? "");
+  await page.type("#password", process.env.PASSWORD ?? "");
+  await page.click("#Login");
+
+  await page.click("text=勤怠打刻");
+  await page.click("text=出社");
+});
